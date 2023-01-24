5902bc47d5f8a187eee1ad714cdad41c8352734a
mando222 <mando222@mando222s-MacBook-Pro.local>
2023-01-24 15:28:16 -0500

    bad code adding electron

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,19 @@
 import ReactDOM from 'react-dom/client';
+import {app, BrowserWindow} from 'electron';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<h1>Hello, world!</h1>);
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
